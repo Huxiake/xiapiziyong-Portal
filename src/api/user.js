cbeb84010714c77c1e404ba7b6efcac6e56236d2
mengli <mengli@qingmutec.com>
2019-05-17 19:32:21 +0800

    init

---
+++ src/api/user.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/login/?phone=' + data.phone + '&password=' + data.password,
+    method: 'post'
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
